5b8c3c78aa7af37d696da244867751313f919ff9
eli1711 <elihofnimariano@gmail.com>
2025-11-19 21:15:45 -0300

    correcao

--- med novo/med novo/med/medpro/src/main/java/com/medpro/medpro/infra/exceptions/security/TratadorDeErros.java
+++ med novo/med novo/med/medpro/src/main/java/com/medpro/medpro/infra/exceptions/security/TratadorDeErros.java
@@ -1,34 +1,34 @@
 package com.medpro.medpro.infra.exceptions.security;
 
 import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.validation.FieldError;
 
 @RestControllerAdvice
 public class TratadorDeErros {
 
-
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Void> tratarError404(){
+    public ResponseEntity<Void> tratarError404() {
         return ResponseEntity.notFound().build();
     }
 
-     public ResponseEntity<List<TratadorDeErros.DadosErroValidacao>> tratarErro400(
-            MethodArgumentNotValidException e){
-                var erro = e.getFieldError();
-                return ResponseEntity
-                .badRequest()
-                .body(erro.stream().map(DadosErroValidacao::new).toList());
-        }
-
-    public record DadosErroValidacao(String campo, String mensagem){
-        this(erro.getField(), erro.getDefaultMessage());
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException e) {
+        List<FieldError> erros = e.getBindingResult().getFieldErrors();
+        List<DadosErroValidacao> dadosErroValidacao = erros.stream()
+                .map(erro -> new DadosErroValidacao(erro.getField(), erro.getDefaultMessage()))
+                .toList();
+        return ResponseEntity.badRequest().body(dadosErroValidacao);
     }
 
+    public record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(String campo, String mensagem) {
+            this.campo = campo;
+            this.mensagem = mensagem;
+        }
+    }
 }
